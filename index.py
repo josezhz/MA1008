ed647b09e6ca9a322087c7243068f3af0b18c506
josezhz <jose13467958@gmail.com>
2022-11-03 23:56:10 +0800

    Allowed user input using turtle's screen.textinput

--- index.py
+++ index.py
@@ -1,76 +1,61 @@
 import turtle
 
-'''
-def intInput(prompt):
+def tryIntInput(title, prompt, default=None, minval=0, maxval=None):
     while True:
         try:
-            x = int(input(prompt))
+            x = int(screen.numinput(title, prompt, default, minval, maxval))
             return x
         except:
-            print("!Please enter an INTEGER")
-
-
-def floatInput(prompt):
+            print("Please enter an INTEGER")
+def tryFloatInput(title, prompt, default=None, minval=0, maxval=None):
     while True:
         try:
-            x = float(input(prompt))
+            x = float(screen.numinput(title, prompt, default, minval, maxval))
             return x
         except:
-            print("!Please enter a NUMBER")
-
-
-def limitedFloatInput(prompt, upperLimit, lowerLimit=0):
-    while True:
-        x = floatInput(prompt)
-        if lowerLimit <= x <= upperLimit:
-            return x
-        else:
-            print(f"!Input must be between {lowerLimit} and {upperLimit}")
-
-
-_L = floatInput("L(mm) = ")
-while True:
-    beamType = intInput("Beam type?\n1. Simply Supported\n2. Overhanging\n3. Cantilever\n")
-    if beamType in [1, 2, 3]:
-        break
-    else:
-        print("!Please choose from 1/2/3")
-loads = []
-while True:
-    while True:
-        loadType = intInput("Load type?\n1. concentrated load\n2. uniformly distributed load\n3. bending moment\n")
-        if loadType == 1:
-            _P = floatInput("P(N) = ")
-            _x = limitedFloatInput("x(mm) = ", _L)
-            loads.append([loadType, _P, _x])
-            break
-        elif loadType == 2:
-            _w = floatInput("w(N/m) = ")
-            _x1 = limitedFloatInput("x1(mm) = ", _L)
-            _x2 = limitedFloatInput("x2(mm) = ", _L, _x1)
-            loads.append([loadType, _w, _x1, _x2])
-            break
-        elif loadType == 3:
-            _M = floatInput("M(Nm) = ")
-            _x = limitedFloatInput("x(mm) = ", _L)
-            loads.append([loadType, _M, _x])
-            break
-    if input("Add another load? (y/n)").lower() == "n":
-        break
-print("_L =", _L, "beamType =", beamType)
-print("loads =", loads)
-
-data = open("data.txt", "w")
-print(_L, beamType, file=data)
-for eachLoad in loads:
-    print(" ".join(str(e) for e in eachLoad), file=data)
-data.close()
-'''
+            print("Please enter a NUMBER")
 
 screen = turtle.Screen()
 screen.setup(550, 750)
 screen.setworldcoordinates(-5, 0, 105, 150)
-turtle.speed(10)
+turtle.speed(0)
+
+beamType = tryIntInput("Beam Type", "Beam type:\n1. Simply supported\n2. Overhanging\n3. Cantilever\nPlease enter an INTEGER", None, 1, 3)
+_L = tryFloatInput("Beam Length", "L(mm) =", 1000, 0)
+loads = []
+while True:
+    n = len(loads) + 1
+    print("n =", n)
+    loadType = tryIntInput(f"Load #{n}", "Load type:\n1. Concentrated load\n2. Uniformly distributed load\n3. Bending moment\nPlease enter an INTEGER", None, 1, 3)
+    if loadType == 1:
+        _P = tryFloatInput(f"Load #{n}", "P(N) =")
+        _x = tryFloatInput(f"Load #{n}", "x(mm) =", None, 0, _L)
+        loads.append([loadType, _P, _x])
+    elif loadType == 2:
+        _w = tryFloatInput(f"Load #{n}", "w(N/m) =")
+        _x1 = tryFloatInput(f"Load #{n}", "x1(mm) =", None, 0, _L)
+        _x2 = tryFloatInput(f"Load #{n}", "x2(mm) =", None, _x1, _L)
+        loads.append([loadType, _w, _x1, _x2])
+    elif loadType == 3:
+        _M = tryFloatInput(f"Load #{n}", "M(Nm) =")
+        _x = tryFloatInput(f"Load #{n}", "x(mm) =", None, 0, _L)
+        loads.append([loadType, _M, _x])
+    if not tryIntInput(f"Add Load #{n+1}?", "1: Yes\n0: No", None, 0, 1):
+        break
+print("_L =", _L, "beamType =", beamType)
+print("loads =", loads)
+
+dataFile = open("data.txt", "w")
+print(beamType, _L, file=dataFile)
+for eachLoad in loads:
+    print(" ".join(str(e) for e in eachLoad), file=dataFile)
+dataFile.close()
+
+
+# screen = turtle.Screen()
+# screen.setup(550, 750)
+# screen.setworldcoordinates(-5, 0, 105, 150)
+# turtle.speed(0)
 
 turtle.up()
 turtle.goto(-5, 0)
@@ -88,10 +73,10 @@ turtle.up()
 turtle.goto(-5, 50)
 turtle.down()
 turtle.forward(110)
-
 turtle.up()
 turtle.goto(0, 123)
 turtle.down()
+turtle.fillcolor("lightgray")
 turtle.begin_fill()
 turtle.forward(100)
 turtle.left(90)
@@ -101,15 +86,38 @@ turtle.forward(100)
 turtle.left(90)
 turtle.forward(4)
 turtle.end_fill()
+turtle.left(90)
 
-turtle.begin_fill()
-turtle.right(30)
-turtle.forward(5)
-turtle.left(120)
-turtle.forward(5)
-turtle.left(120)
-turtle.forward(5)
-turtle.end_fill()
+dataFile = open("test.txt", "r")
+lines = dataFile.readlines()
+dataFile.close()
+beamData = lines[0]
+loadsData = lines[1:]
+
+beamType = beamData[0]
+if beamType == "1": # simply supported
+    turtle.begin_fill()
+    turtle.right(120)
+    turtle.forward(5)
+    turtle.left(120)
+    turtle.forward(5)
+    turtle.left(120)
+    turtle.forward(5)
+    turtle.end_fill()
+    turtle.right(120)
+
+    turtle.up()
+    turtle.forward(100)
+    turtle.down()
+    turtle.right(180)
+    turtle.begin_fill()
+    turtle.circle(2.5)
+    turtle.end_fill()
+    turtle.right(180)
+elif beamType == "2":
+    print("Overhanging beam under development")
+elif beamType == "3":
+    print("Cantilever beam under development")
 
 screen.exitonclick()
 turtle.done()
